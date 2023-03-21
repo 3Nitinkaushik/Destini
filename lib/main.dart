aaf288d60f17c6c9277b666e5149c4f6c212c1fa
nitin <3nitinkaushik@gmail.com>
2023-03-22 03:04:31 +0530

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'storybrain.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -10,14 +10,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("DESTINI",style: TextStyle(fontSize: 20,color: Colors.blue,backgroundColor: Colors.white),),
-          backgroundColor: Colors.white70,
-        ),
-        body: Quiz(),
-      ),
+    return const MaterialApp(
+      home: Quiz()
     );
   }
 }
@@ -27,6 +21,77 @@ class Quiz extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(image: AssetImage('images/back.jpg'),fit: BoxFit.cover)
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: const Center(child: Text("DESTINI",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.blue),)),
+
+        ),
+        body: const Destinibody(),
+      ),
+    );
+  }
+}
+
+class Destinibody extends StatefulWidget {
+  const Destinibody({Key? key}) : super(key: key);
+
+  @override
+  State<Destinibody> createState() => _DestinibodyState();
+}
+
+class _DestinibodyState extends State<Destinibody> {
+  Storybrain storyb= Storybrain();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+         Expanded(flex:5,
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(storyb.getstorytitle(),style: TextStyle(fontSize: 25,color: Colors.white),
+              textAlign: TextAlign.center,),
+            ),
+          )
+          ,),
+         Expanded(child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: TextButton(onPressed: (){setState(() {
+            storyb.nextstory(1);
+
+          });},
+            child: Text(storyb.getstorychoice1(),
+            style: TextStyle(fontSize: 20,color: Colors.white),
+          ),
+            style: TextButton.styleFrom(backgroundColor: Colors.teal),
+          ),
+        ),
+        ),
+        Expanded(child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Visibility(
+            visible: storyb.buttonvis(),
+            child: TextButton(onPressed: (){setState(() {
+              storyb.nextstory(2);
+
+            });}, child: Text(storyb.getstorychoice2(),
+              style: TextStyle(fontSize: 20,color: Colors.white),
+            ),
+              style: TextButton.styleFrom(backgroundColor: Colors.blueAccent),
+            ),
+
+          ),
+        ),
+        ),
+      ],
+    );
   }
 }
